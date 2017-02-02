7a382f67d2b84644f74e774337ad5db79bc037d5
Denton Liu <liu.denton@gmail.com>
2017-02-02 14:28:33 -0800

    Download bashrc_sources externally

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-mkdir -p bashrc_sources
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o bashrc_sources/git-prompt.sh
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o bashrc_sources/git-completion.bash
-curl https://raw.githubusercontent.com/Bash-it/bash-it/master/completion/available/tmux.completion.bash -o bashrc_sources/tmux.completion.bash
+SOURCE_FOLDER=${SSHHOME:-~/.config-files}/bashrc_sources
+mkdir -p $SOURCE_FOLDER
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o $SOURCE_FOLDER/git-prompt.sh
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o $SOURCE_FOLDER/git-completion.bash
+curl https://raw.githubusercontent.com/Bash-it/bash-it/master/completion/available/tmux.completion.bash -o $SOURCE_FOLDER/tmux.completion.bash
