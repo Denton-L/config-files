1c077264b2e286a039a61d7105298df038afc162
Denton Liu <liu.denton@gmail.com>
2017-08-02 22:19:41 -0400

    Preserve old files instead of deleting

--- install.sh
+++ install.sh
@@ -24,14 +24,14 @@ i3
 
 for f in $dotfiles
 do
-	[ -d ~/.$f ] && rm -rf ~/.$f
+	[ -d ~/.$f ] && mv ~/.$f ~/.$f.old
 	ln -sf $PWD/$f ~/.$f
 done
 
 mkdir -p ~/.config
 for f in $configfiles
 do
-	[ -d ~/.config/$f ] && rm -rf ~/.config/$f
+	[ -d ~/.config/$f ] && mv ~/.config/$f ~/.config/$f.old
 	ln -sf $PWD/$f ~/.config/$f
 done
 
