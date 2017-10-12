733be67fda53aeca3387895c3b27da5bc5357274
Denton Liu <liu.denton@gmail.com>
2017-10-12 18:47:20 -0400

    Ensures completions exist before sourcing

--- completions.bash
+++ completions.bash
@@ -1,41 +1,54 @@
-_git_squash() {
-	if [[ "$COMP_CWORD" == "2" ]]
-	then
-		__git_complete_revlist
-	fi
+completions() {
+	command -v _git_complete_revlist &&
+	_git_squash() {
+		if [[ "$COMP_CWORD" == "2" ]]
+		then
+			__git_complete_revlist
+		fi
 
-	return 0
-} &&
-complete -F _git_squash git-squash "git squash"
+		return 0
+	} &&
+	complete -F _git_squash git-squash "git squash"
 
-_git_delete() {
-	if [[ "$COMP_CWORD" == "2" ]]
-	then
-		__gitcomp_nl "$(__git_remotes)"
-	else
-		__gitcomp_nl "$(__git_heads)"
-	fi
+	command -v __gitcomp_nl &&
+	command -v __git_remotes &&
+	command -v __git_heads &&
+	_git_delete() {
+		if [[ "$COMP_CWORD" == "2" ]]
+		then
+			__gitcomp_nl "$(__git_remotes)"
+		else
+			__gitcomp_nl "$(__git_heads)"
+		fi
 
-	return 0
-} &&
-complete -F _git_delete git-delete "git delete"
+		return 0
+	} &&
+	complete -F _git_delete git-delete "git delete"
 
-_git_squash_rebase() {
-	if [[ "$COMP_CWORD" == "2" ]]
-	then
-		__git_complete_revlist
-	fi
+	command -v __git_complete_revlist &&
+	_git_squash_rebase() {
+		if [[ "$COMP_CWORD" == "2" ]]
+		then
+			__git_complete_revlist
+		fi
 
-	return 0
-} &&
-complete -F _git_squash_rebase git-squash-rebase "git squash-rebase"
+		return 0
+	} &&
+	complete -F _git_squash_rebase git-squash-rebase "git squash-rebase"
 
-_git_git() {
-	__git_compute_all_commands
-	__gitcomp "$__git_all_commands $(__git_aliases)"
-} &&
-complete -F _git_git git-git "git git"
+	command -v __git_compute_all_commands &&
+	command -v __gitcomp &&
+	command -v __git_all_commands &&
+	command -v __git_aliases &&
+	_git_git() {
+		__git_compute_all_commands
+		__gitcomp "$__git_all_commands $(__git_aliases)"
+	} &&
+	complete -F _git_git git-git "git git"
 
-complete -F _ssh chromium-socks
+	complete -F _ssh chromium-socks
 
-complete -F _command run
+	command -v _command && complete -F _command run
+}
+
+completions &> /dev/null
