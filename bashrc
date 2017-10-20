Denton-L/config-files

commit 0b7464a50271af147733e2419e93aa8c4f39a984

Add quotes for scripts

bashrc
@@ -1,12 +1,18 @@
-CONFIG_ROOT=${SSHHOME:-~}
+if [[ -z "$SSHHOME" ]]
+then
+	CONFIG_ROOT=~
+else
+	CONFIG_ROOT="$SSHHOME"
+fi
+
 shopt -s autocd checkwinsize checkjobs extglob
 
-[[ -d $CONFIG_ROOT/.config-files/bashrc_sources/ ]] && for f in $CONFIG_ROOT/.config-files/bashrc_sources/*
+[[ -d "$CONFIG_ROOT/.config-files/bashrc_sources/" ]] && for f in "$CONFIG_ROOT/.config-files/bashrc_sources/*"
 do
-	[[ -f $f ]] && source $f
+	[[ -f "$f" ]] && source "$f"
 done
 
-[[ -f $CONFIG_ROOT/bin/completions.bash ]] && source $CONFIG_ROOT/bin/completions.bash
+[[ -f "$CONFIG_ROOT/bin/completions.bash" ]] && source "$CONFIG_ROOT/bin/completions.bash"
 
 cdup() {
 	cd "$(pwd | sed "s/\(.*\/$1\/\).*/\1/")"

update.bash
@@ -1,0 +1,7 @@
+#!/usr/bin/env bash
+
+SOURCE_FOLDER=${SSHHOME:-~/.config-files}/bashrc_sources
+mkdir -p "$SOURCE_FOLDER"
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o "$SOURCE_FOLDER/git-prompt.sh"
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o "$SOURCE_FOLDER/git-completion.bash"
+curl https://raw.githubusercontent.com/Bash-it/bash-it/master/completion/available/tmux.completion.bash -o "$SOURCE_FOLDER/tmux.completion.bash"

update.sh
@@ -1,7 +1,0 @@
-#!/bin/sh
-
-SOURCE_FOLDER=${SSHHOME:-~/.config-files}/bashrc_sources
-mkdir -p $SOURCE_FOLDER
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o $SOURCE_FOLDER/git-prompt.sh
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o $SOURCE_FOLDER/git-completion.bash
-curl https://raw.githubusercontent.com/Bash-it/bash-it/master/completion/available/tmux.completion.bash -o $SOURCE_FOLDER/tmux.completion.bash

install.bash
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 readonly dotfiles='
 Xmodmap
@@ -25,15 +25,15 @@ i3
 
 for f in $dotfiles
 do
-	[ -d ~/.$f ] && rm -rf ~/.$f
-	ln -sf $PWD/$f ~/.$f
+	[[ -d ~/."$f" ]] && rm -rf ~/."$f"
+	ln -sf "$PWD/$f" ~/."$f"
 done
 
 mkdir -p ~/.config
 for f in $configfiles
 do
-	[ -d ~/.config/$f ] && rm -rf ~/.config/$f
-	ln -sf $PWD/$f ~/.config/$f
+	[[ -d ~/.config/"$f" ]] && rm -rf ~/.config/"$f"
+	ln -sf "$PWD/$f" ~/.config/"$f"
 done
 
-./update.sh
+./update.bash

sshrc.d/profile
@@ -1,11 +1,1 @@
-umask 0077
-
-export PATH=~/bin:~/.local/bin:$PATH
-export GOPATH=~/go
-export VISUAL=vim
-export EDITOR=$VISUAL
-export HISTCONTROL=ignoredups
-export GIT_PS1_SHOWDIRTYSTATE=1
-export GIT_PS1_SHOWSTASHSTATE=1
-export GIT_PS1_SHOWUNTRACKEDFILES=1
-export GIT_PS1_SHOWUPSTREAM=auto
+../profile

commit aa1d3c24f67c4d84d27dce721b0d3078e636191b

Fix scripts broken by 0b7464a5

bashrc
@@ -7,12 +7,12 @@ fi
 
 shopt -s autocd checkwinsize checkjobs extglob
 
-[[ -d "$CONFIG_ROOT/.config-files/bashrc_sources/" ]] && for f in "$CONFIG_ROOT/.config-files/bashrc_sources/*"
+[[ -d "$CONFIG_ROOT"/.config-files/bashrc_sources/ ]] && for f in "$CONFIG_ROOT"/.config-files/bashrc_sources/*
 do
 	[[ -f "$f" ]] && source "$f"
 done
 
-[[ -f "$CONFIG_ROOT/bin/completions.bash" ]] && source "$CONFIG_ROOT/bin/completions.bash"
+[[ -f "$CONFIG_ROOT"/bin/completions.bash ]] && source "$CONFIG_ROOT"/bin/completions.bash
 
 cdup() {
 	cd "$(pwd | sed "s/\(.*\/$1\/\).*/\1/")"

sshrc
@@ -1,11 +1,11 @@
-bind -f $SSHHOME/.sshrc.d/inputrc
-. $SSHHOME/.sshrc.d/bash_profile
-. $SSHHOME/.sshrc.d/bashrc
+bind -f "$SSHHOME"/.sshrc.d/inputrc
+. "$SSHHOME"/.sshrc.d/bash_profile
+. "$SSHHOME"/.sshrc.d/bashrc
 
-$SSHHOME/.sshrc.d/update.sh &> /dev/null
-for f in $SSHHOME/bashrc_sources/*
+"$SSHHOME"/.sshrc.d/update.bash > /dev/null 2>&1
+for f in "$SSHHOME"/bashrc_sources/*
 do
-	. $f
+	. "$f"
 done
 
 export VIMINIT='let $MYVIMRC="$SSHHOME/.sshrc.d/vimrc" | source $MYVIMRC'

sshrc.d/update.bash
@@ -1,0 +1,1 @@
+../update.bash

sshrc.d/update.sh
@@ -1,1 +1,0 @@
-../update.sh

update.bash
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-SOURCE_FOLDER=${SSHHOME:-~/.config-files}/bashrc_sources
+if [[ -z "$SSHHOME" ]]
+then
+	SOURCE_FOLDER=~/.config-files
+else
+	SOURCE_FOLDER="$SSHHOME"
+fi
+SOURCE_FOLDER="$SOURCE_FOLDER"/bashrc_sources
+
 mkdir -p "$SOURCE_FOLDER"
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o "$SOURCE_FOLDER/git-prompt.sh"
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o "$SOURCE_FOLDER/git-completion.bash"