bdedacdc4ceaa439e3a9400de0f1a284759ad2c1
Denton Liu <liu.denton@gmail.com>
2019-01-31 22:48:54 -0800

    Download correct git completion version

--- update.bash
+++ update.bash
@@ -9,6 +9,9 @@ fi
 SOURCE_FOLDER="$SOURCE_FOLDER"/bashrc_sources
 
 mkdir -p "$SOURCE_FOLDER"
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o "$SOURCE_FOLDER/git-prompt.sh"
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o "$SOURCE_FOLDER/git-completion.bash"
+
+GIT_VERSION=v$(git --version | sed 's/git version \([0-9]\+\.[0-9]\+\.[0-9]\+\).*/\1/')
+
+curl https://raw.githubusercontent.com/git/git/$GIT_VERSION/contrib/completion/git-prompt.sh -o "$SOURCE_FOLDER/git-prompt.sh"
+curl https://raw.githubusercontent.com/git/git/$GIT_VERSION/contrib/completion/git-completion.bash -o "$SOURCE_FOLDER/git-completion.bash"
 curl https://raw.githubusercontent.com/Bash-it/bash-it/master/completion/available/tmux.completion.bash -o "$SOURCE_FOLDER/tmux.completion.bash"
