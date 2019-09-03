f33048df630fc4e7f0660ddea26bc1bcde4a70a8
Denton Liu <liu.denton@gmail.com>
2019-09-03 11:59:37 -0700

    Fix update.sh for Macs

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ SOURCE_FOLDER="$SOURCE_FOLDER"/bashrc_sources
 
 mkdir -p "$SOURCE_FOLDER"
 
-GIT_VERSION=v$(git --version | sed 's/git version \([0-9]\+\.[0-9]\+\.[0-9]\+\).*/\1/')
+GIT_VERSION=v$(git --version | sed 's/git version \([0-9]*\.[0-9]*\.[0-9]*\).*/\1/')
 
 curl https://raw.githubusercontent.com/git/git/$GIT_VERSION/contrib/completion/git-prompt.sh -o "$SOURCE_FOLDER/git-prompt.sh"
 curl https://raw.githubusercontent.com/git/git/$GIT_VERSION/contrib/completion/git-completion.bash -o "$SOURCE_FOLDER/git-completion.bash"
