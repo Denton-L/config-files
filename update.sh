d82d7c004dbeacaff0d87b1049497681cf2b0968
Denton Liu <liu.denton@gmail.com>
2019-12-12 22:23:29 -0800

    Lowercase update.sh variable names

--- update.sh
+++ update.sh
@@ -2,16 +2,16 @@
 
 if [[ -z "$SSHHOME" ]]
 then
-	SOURCE_DIR=~/.config-files
+	source_dir=~/.config-files
 else
-	SOURCE_DIR="$SSHHOME"
+	source_dir="$SSHHOME"
 fi
-SOURCE_DIR="$SOURCE_DIR"/bashrc_sources
+source_dir="$source_dir"/bashrc_sources
 
-mkdir -p "$SOURCE_DIR"
+mkdir -p "$source_dir"
 
-GIT_VERSION=v$(git --version | sed 's/git version \([0-9]*\.[0-9]*\.[0-9]*\).*/\1/')
+git_version=v$(git --version | sed 's/git version \([0-9]*\.[0-9]*\.[0-9]*\).*/\1/')
 
-curl https://raw.githubusercontent.com/git/git/$GIT_VERSION/contrib/completion/git-prompt.sh -o "$SOURCE_DIR/git-prompt.sh"
-curl https://raw.githubusercontent.com/git/git/$GIT_VERSION/contrib/completion/git-completion.bash -o "$SOURCE_DIR/git-completion.bash"
-curl https://raw.githubusercontent.com/Bash-it/bash-it/master/completion/available/tmux.completion.bash -o "$SOURCE_DIR/tmux.completion.bash"
+curl https://raw.githubusercontent.com/git/git/$git_version/contrib/completion/git-prompt.sh -o "$source_dir/git-prompt.sh"
+curl https://raw.githubusercontent.com/git/git/$git_version/contrib/completion/git-completion.bash -o "$source_dir/git-completion.bash"
+curl https://raw.githubusercontent.com/Bash-it/bash-it/master/completion/available/tmux.completion.bash -o "$source_dir/tmux.completion.bash"
