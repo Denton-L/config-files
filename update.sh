e34ab8b76dafc8a3550b06ec17e4b4119b4d4e5c
Denton Liu <liu.denton@gmail.com>
2019-12-16 04:32:52 -0800

    Download git-credential-netrc

--- update.sh
+++ update.sh
@@ -2,11 +2,11 @@
 
 if [[ -z "$SSHHOME" ]]
 then
-	source_dir=~/.config-files
+	repo_dir=~/.config-files
 else
-	source_dir="$SSHHOME"
+	repo_dir="$SSHHOME"
 fi
-source_dir="$source_dir"/bashrc_sources
+source_dir="$repo_dir"/bashrc_sources
 
 mkdir -p "$source_dir"
 
@@ -15,3 +15,5 @@ git_version=v$(git --version | sed 's/git version \([0-9]*\.[0-9]*\.[0-9]*\).*/\
 curl https://raw.githubusercontent.com/git/git/$git_version/contrib/completion/git-prompt.sh -o "$source_dir/git-prompt.sh"
 curl https://raw.githubusercontent.com/git/git/$git_version/contrib/completion/git-completion.bash -o "$source_dir/git-completion.bash"
 curl https://raw.githubusercontent.com/Bash-it/bash-it/master/completion/available/tmux.completion.bash -o "$source_dir/tmux.completion.bash"
+curl https://raw.githubusercontent.com/gitster/git/master/contrib/credential/netrc/git-credential-netrc -o "$repo_dir/bin/git-credential-netrc"
+chmod +x "$repo_dir/bin/git-credential-netrc"
