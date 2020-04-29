8c6468a9d179a234453bbbf939c6e4fdec95c581
Denton Liu <liu.denton@gmail.com>
2020-04-29 05:54:28 -0400

    update.sh: update git-credential-netrc location

--- update.sh
+++ update.sh
@@ -15,5 +15,5 @@ git_version=v$(git --version | sed 's/git version \([0-9]*\.[0-9]*\.[0-9]*\).*/\
 curl https://raw.githubusercontent.com/git/git/$git_version/contrib/completion/git-prompt.sh -o "$source_dir/git-prompt.sh"
 curl https://raw.githubusercontent.com/git/git/$git_version/contrib/completion/git-completion.bash -o "$source_dir/git-completion.bash"
 curl https://raw.githubusercontent.com/Bash-it/bash-it/master/completion/available/tmux.completion.bash -o "$source_dir/tmux.completion.bash"
-curl https://raw.githubusercontent.com/gitster/git/master/contrib/credential/netrc/git-credential-netrc -o "$repo_dir/bin/git-credential-netrc"
+curl https://raw.githubusercontent.com/gitster/git/master/contrib/credential/netrc/git-credential-netrc.perl -o "$repo_dir/bin/git-credential-netrc"
 chmod +x "$repo_dir/bin/git-credential-netrc"
